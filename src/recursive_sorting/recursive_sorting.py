80b44db1ef78a59391b768b42fa96fc09e1a3cf3
cwern40 <chris.wernli@hotmail.com>
2020-02-29 13:20:53 -0700

    I finished the merge helper function

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,7 +3,26 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    if len(merged_arr) == 2:
+        if arrA[0] < arrB[0]:
+            merged_arr = [arrA[0], arrB[0]]
+        else:
+            merged_arr = [arrB[0], arrA[0]]
+    else:
+        merged_arr = []
+        j = 0
+        i = 0
+        while j != len(arrB) and i != len(arrA):
+            if arrA[i] < arrB[j]:
+                merged_arr.append(arrA[i])
+                i += 1
+                if i == len(arrA):
+                    merged_arr += arrB[j:]
+            else:
+                merged_arr.append(arrB[j])
+                j += 1
+                if j == len(arrB):
+                    merged_arr += arrA[i:]
     return merged_arr
 
 
